a5813c00fd35336ff9db4bd84ee7824bf03c1450
Omni <haosgypsy@gmail.com>
2016-02-10 04:36:15 +0900

    Update chrome_in_eng.bat

--- bats/chrome_in_eng.bat
+++ bats/chrome_in_eng.bat
@@ -1,2 +1,6 @@
 echo "You need to modify the path and filename ...
 rem path/to/chrome/chrome.exe --lang=en
+rem @echo off
+rem start C:\Users\Administrator\Desktop\apps\GoogleChromePortable\GoogleChromePortable.exe --lang=en
+pause
+exit
